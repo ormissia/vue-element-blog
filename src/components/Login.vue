8bfe45ad04d41db30615983c0565d5c5d10778bf
宋奕锟 <ormissia@outlook.com>
2020-09-24 17:44:54 +0800

    init project

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: white;
+  border-radius: 5px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+// 导入全局样式
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
