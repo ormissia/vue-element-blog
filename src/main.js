c8fe117e4cb12638d949882d5aef64a44c9f5fca
安红豆 <ormissia@outlook.com>
2020-12-29 23:36:15 +0800

    -

--- src/main.js
+++ src/main.js
@@ -13,11 +13,11 @@ import axios from 'axios'
 // 导入qs，用于序列化发送参数
 import qs from 'qs'
 
-// InfiniteScroll无限滚动
-import infiniteScroll from 'vue-infinite-scroll'
-
 // 引入MD5
 import md5 from 'js-md5'
+
+// InfiniteScroll无限滚动
+import infiniteScroll from 'vue-infinite-scroll'
 
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8085/api/'
